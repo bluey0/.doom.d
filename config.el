110b32873293f947f6502270157c9c663e04a177
blueywoons <lcd359.khoa@gmail.com>
2021-09-02 21:39:23 +0700

    org-roam + cleanup

--- config.el
+++ config.el
@@ -1,59 +1,19 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-one)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "bluey"
+(setq user-full-name "blueywoons"
       user-mail-address "lcd359.khoa@gmail.com")
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org-files/")
+(setq org-directory-hub "~/org-files/")
+(setq org-directory (concat org-directory-hub "notes/"))
 (setq org-agenda-files (list org-directory))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type nil)
-
-;; set org-books directory
-(setq org-books-file "~/org-files/books.org")
 
 ;; set the theme
 (setq doom-theme 'doom-gruvbox)
@@ -78,14 +38,10 @@
 (setq mouse-wheel-scroll-amount '(0.07))
 (setq mouse-wheel-progressive-speed nil)
 ;; scrolling doesn't fuck up the cursor
-(setq scroll-preserve-screen-position nil)
 
-;; open magit on yadm repo by evaluating (magit-status "/yadm::")
-(require 'tramp)
-(add-to-list 'tramp-methods
- '("yadm"
-   (tramp-login-program "yadm")
-   (tramp-login-args (("enter")))
-   (tramp-login-env (("SHELL") ("/bin/sh")))
-   (tramp-remote-shell "/bin/sh")
-   (tramp-remote-shell-args ("-c"))))
+;; org-roam
+(setq org-roam-directory (concat org-directory-hub "roam/"))
+(setq org-roam-capture-templates
+      '(("n" "default" plain "%?" :if-new
+         (file+head "%<%Y%m%d%H%M%S>.org" "#+title: ${title}\n")
+         :unnarrowed t)))
